06fbb8364271692fe10afc1cc24ea7132a6bb8d7
Taraneshwer <Taraneshwer.sankar@gmail.com>
2026-07-23 19:57:10 +0530

    .

--- scripts/check-npm.cjs
+++
@@ -1,5 +0,0 @@
-const userAgent = process.env.npm_config_user_agent || '';
-if (!userAgent.startsWith('npm/')) {
-  console.error('This workspace is configured for npm. Please run the install commands with npm.');
-  process.exit(1);
-}
--- scripts/post-merge.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-npm install
-npm --workspace lib/db run push
