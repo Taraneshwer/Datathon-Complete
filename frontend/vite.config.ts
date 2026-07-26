11d4bcb19bd7e84c5ddb16291f216f579b777e8d
Parikshith Lakshmikanth <parikshithlak@gmail.com>
2026-07-26 22:52:17 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
     },
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8000',
+        target: process.env.VITE_API_URL || 'https://crime-intel-backend-50044277156.development.catalystappsail.in',
         changeOrigin: true,
       }
     }
